f567e4709b3cef318f686e274240e594a1cf619a
Ammar <Ammar12@gmail.com>
2024-06-30 17:29:50 +0300

    df

---
+++ js/159.c93c389d.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunkrealestate"]=self["webpackChunkrealestate"]||[]).push([[159],{1159:function(e,t,n){n.r(t),n.d(t,{default:function(){return i}});var a=n(6768);const r={class:""},s=(0,a.Lk)("div",{class:"w-[95%] mx-auto flex flex-col"},null,-1),u=[s];function l(e,t){return(0,a.uX)(),(0,a.CE)("div",r,u)}var c=n(1241);const f={},o=(0,c.A)(f,[["render",l]]);var i=o}}]);
+//# sourceMappingURL=159.c93c389d.js.map
